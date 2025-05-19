aa7fed9e86dc4c9d56bcb65134865cc39654bb27
iniaaqilah <iniaaqilah@gmail.com>
2025-05-20 04:55:53 +0700

    login 1

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const authRoutes = require('./routes/authRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const userRoutes = require('./routes/userRoutes');
-
+const express = require("express");
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+const path = require("path");
 
-app.use(session({
-    secret: 'rahasia123',
-    resave: false,
-    saveUninitialized: true
-}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-// Routes
-app.use('/', authRoutes);
-app.use('/booking', bookingRoutes);
-app.use('/user', userRoutes);
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
 });
