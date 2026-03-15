5fc47607c78fd6b548ba7e282921bb51ebf6a3ea
wina <winarahmalia189@gmail.com>
2026-03-16 06:59:48 +0700

    Update db to aiven and ready to deploy

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'hotel'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
