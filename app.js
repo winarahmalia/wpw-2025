d7055299c14f5338de38ed22b186eddd1256ca2c
wina <winarahmalia189@gmail.com>
2026-03-16 09:12:57 +0700

    fix: app definition and export for vercel

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 const userRoutes = require('./routes/userRoutes');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -25,4 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
+
+module.exports = app;
 });
